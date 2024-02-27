2e5faa46513850e2ef5a5e8a0c45094b69d35ba0
Helixjoe <joeljohn5112@gmail.com>
2024-02-27 20:42:45 +0530

    Header,Button,Tasks components created

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="onClick()":style="{background:color}" class="btn">{{ task }}</button>
+</template>
+
+<script>
+    export default{
+        name:'Button',
+        props:{
+            task:String,
+            color: String
+        },
+        methods:{
+            onClick(){
+                console.log('clicked')
+            }
+        }
+    }
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,12 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">{{ task.name }}</div>
+</template>
+
+<script>
+    export default{
+        name:'Tasks',
+        props:{
+            tasks:Array
+        }
+    }
+</script>
